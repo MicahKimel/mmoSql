754218f430c339215e412811c831ea183e3e484d
Micah Kimel <micahkimel@Micahs-MacBook-Pro.local>
2022-06-18 11:58:55 -0400

    stored procedures added

---
+++ StoredProcedures/get_user.sql
@@ -0,0 +1,17 @@
+USE `db`;
+DROP procedure IF EXISTS `get_user`;
+
+DELIMITER $$
+USE `db`$$
+CREATE PROCEDURE `get_user` (
+	IN username VARCHAR(45),
+    IN password VARCHAR(45)
+)
+BEGIN
+	select * from db.users where
+    username = username and
+    password = password;
+END$$
+
+DELIMITER ;
+
