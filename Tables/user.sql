4325303a7956b0151942228370f0051a4131906d
Micah Kimel <micahkimel@Micahs-MacBook-Pro.local>
2022-06-20 12:34:32 -0400

    password length

--- Tables/user.sql
+++ Tables/user.sql
@@ -1,7 +1,7 @@
 CREATE TABLE `db`.`users` (
   `userid` INT NOT NULL AUTO_INCREMENT,
   `username` VARCHAR(45) NOT NULL,
-  `password` VARCHAR(45) NOT NULL,
+  `password` VARCHAR(255) NOT NULL,
   `email` VARCHAR(45) NULL,
   `phone` VARCHAR(45) NULL,
   `creationdate` DATETIME DEFAULT (CURRENT_TIMESTAMP()),
