5181b901d4cfce9115af1c2869536745904c1ba6
Micah Kimel <micah.kimel@my.wheaton.edu>
2022-07-27 12:29:48 -0400

    get groups

---
+++ StoredProcedures/get_groups.sql
@@ -0,0 +1,13 @@
+USE `db`;
+DROP procedure IF EXISTS `get_groups`;
+
+DELIMITER $$
+USE `db`$$
+CREATE PROCEDURE `get_groups` (
+    IN idUser INT
+)
+BEGIN
+    select * from group where idUser = idUser
+END$$
+
+DELIMITER ;
