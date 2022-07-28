75fd402f319d7bd2e52d63a4dab9b5d73ca9973a
Micah Kimel <micah.kimel@my.wheaton.edu>
2022-07-28 12:52:52 -0400

    remove extra data

--- StoredProcedures/update_userlocation.sql
+++ StoredProcedures/update_userlocation.sql
@@ -18,13 +18,3 @@ BEGIN
 END$$
 
 DELIMITER ;
-
-CREATE TABLE `db`.`userlocation` (
-  `userid` INT NOT NULL,
-  `xlocation` DECIMAL(10,2) NULL,
-  `ylocation` DECIMAL(10,2) NULL,
-  `zlocation` DECIMAL(10,2) NULL,
-  `xrotation` DECIMAL(4,2) NULL,
-  `updatedate` DATETIME DEFAULT (CURRENT_TIMESTAMP()),
-  `creationdate` DATETIME DEFAULT (CURRENT_TIMESTAMP()),
-  CONSTRAINT id_constraint PRIMARY KEY (`userid`));
